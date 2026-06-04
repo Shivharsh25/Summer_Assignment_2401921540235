3d6831716d5cf69e16d5ef78f839fe938a6a28ef
Shivharsh <shivharshtiwari86017@gmail.com>
2026-06-04 14:21:02 +0530

    Week 1 Day 4 solutions

---
+++ Week_1/Day_4/matrixDiagonalSum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            s += mat[i][i];             
+            s += mat[i][n - 1 - i];    
+        }
+        if (n % 2 != 0)
+            s -= mat[n / 2][n / 2];
+        return s;
+    }
+};
