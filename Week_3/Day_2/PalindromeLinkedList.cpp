637ec6eae5dde48fd29e7091e64b27d4fa7dd5f6
Shivharsh <shivharshtiwari86017@gmail.com>
2026-06-16 08:35:35 +0530

    Week 3 Day 2 solutions

---
+++ Week_3/Day_2/PalindromeLinkedList.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr = head, *prev = NULL, *Next = NULL;
+        while (curr) {
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+    }
+     bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverseList(slow->next);
+        ListNode* a = head, *b = newHead;
+        while(b){
+            if(a->val != b->val) return false;
+            a = a->next;
+            b = b->next;
+        }
+        return true;
+    }
+};
